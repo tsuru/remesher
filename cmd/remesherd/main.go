83a49b97653c1d23efc27fce96219e38d85f1ec3
André Carvalho <asantostc@gmail.com>
2018-03-02 16:54:58 -0300

    cmd: add flags for num workers and log level

--- cmd/remesherd/main.go
+++ cmd/remesherd/main.go
@@ -22,38 +22,47 @@ func main() {
 		masterURL         string
 		kubeconfig        string
 		neighborhoodLabel string
+		numWorkers        int
+		logLevel          string
 	)
 
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "Path to a kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&masterURL, "master", "", "The address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster.")
 	flag.StringVar(&neighborhoodLabel, "neighborhood-label", "", "The label to use when grouping nodes in the mesh.")
+	flag.IntVar(&numWorkers, "num-workers", 1, "The number of workers processing the work queue.")
+	flag.StringVar(&logLevel, "log-level", "info", "The log level.")
 	flag.Parse()
 
-	entry := logrus.NewEntry(logrus.New())
+	log := logrus.New()
+	level, err := logrus.ParseLevel(logLevel)
+	if err != nil {
+		log.Fatalf("unable to parse log level %q: %v", logLevel, err)
+	}
+	log.SetLevel(level)
 
 	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeconfig)
 	if err != nil {
-		entry.Fatalf("Error building kubeconfig: %s", err.Error())
+		log.Fatalf("Error building kubeconfig: %s", err.Error())
 	}
 
 	kubeClient, err := kubernetes.NewForConfig(cfg)
 	if err != nil {
-		entry.Fatalf("Error building kubernetes clientset: %s", err.Error())
+		log.Fatalf("Error building kubernetes clientset: %s", err.Error())
 	}
 
 	calicoClient, err := calicoclientv3.NewFromEnv()
 	if err != nil {
-		entry.Fatalf("Error building calico client: %s", err.Error())
+		log.Fatalf("Error building calico client: %s", err.Error())
 	}
 
 	kubeInformerFactory := kubeinformers.NewSharedInformerFactory(kubeClient, time.Second*30)
 
-	ctl := controller.NewController(kubeClient, kubeInformerFactory, entry, neighborhoodLabel, calicoClient)
+	ctl := controller.NewController(kubeClient, kubeInformerFactory, logrus.NewEntry(log), neighborhoodLabel, calicoClient)
 
 	go kubeInformerFactory.Start(stopCh)
 
-	if err = ctl.Run(2, stopCh); err != nil {
-		entry.Fatalf("Error running controller: %s", err.Error())
+	if err = ctl.Run(numWorkers, stopCh); err != nil {
+		log.Fatalf("Error running controller: %s", err.Error())
 	}
 }
 
