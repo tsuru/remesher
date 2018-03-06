51514338942beba322d7e701a539a5d0a0098114
André Carvalho <asantostc@gmail.com>
2018-03-06 16:56:22 -0300

    k8s: adds godoc

--- pkg/k8s/k8s.go
+++ pkg/k8s/k8s.go
@@ -5,6 +5,7 @@ import (
 	"k8s.io/client-go/tools/clientcmd"
 )
 
+// NewClientset create a kubernetes Clientset
 func NewClientset(masterURL, kubeConfig string) (*kubernetes.Clientset, error) {
 	cfg, err := clientcmd.BuildConfigFromFlags(masterURL, kubeConfig)
 	if err != nil {
